5e6258a9c87b8d1265b50c8ecd55b382aece4f64
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-05-28 22:36:10 +0900

    불리언 함수, Falshy 형태값, Trhthy 형태값

---
+++ 7.built_in/3.boolean.js
@@ -0,0 +1,26 @@
+/* 
+    Boolean 
+*/
+
+const isTrue = true;
+console.log(isTrue.valueOf());
+
+/* 
+    Falshy
+    0
+    -0
+    null
+    NaN
+    undefined
+    ''
+*/
+
+/* 
+    Truthy
+    1
+    -1
+    '0'
+    'false'
+    []
+    {}
+*/

c0ec2161769fc82478a9a1ebb2abd2ffec58fddc
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-07-04 00:26:09 +0900

    프로토타입 상속

---
+++ 15.prototype.js/4.inheritance.js
@@ -0,0 +1,39 @@
+/* 
+    프로토타입을 베이스한 객체지향 프로그래밍
+*/
+function Animal(name, emoji) {
+  this.name = name;
+  this.emoji = emoji;
+}
+
+Animal.prototype.printName = function () {
+  console.log(`${this.name} ${this.emoji}`);
+};
+
+function Dog(name, emoji, owner) {
+  Animal.call(this, name, emoji);
+  this.owner = owner;
+}
+
+// Dog.prototype = Object.create(Object.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+Dog.prototype.play = () => {
+  console.log("같이 놀자용!");
+};
+
+function Tiger(name, emoji) {
+  Animal.call(this, name, emoji);
+}
+
+Tiger.prototype = Object.create(Animal.prototype);
+Tiger.prototype.hunt = () => {
+  console.log("사냥하자!");
+};
+const dog1 = new Dog("멍멍", "🐶", "엘리");
+dog1.play();
+dog1.printName();
+
+const tiger1 = new Tiger("어흥", "🐱");
+tiger1.printName();
+tiger1.hunt();

5a76f679f14119abb9160c63e5d054a24009cd4c
wonsickgo <45756634+wonsikgo@users.noreply.github.com>
2022-07-07 16:58:36 +0900

    클로저 퀴즈

---
+++ 16.closure/3.var.js
@@ -0,0 +1,14 @@
+function loop() {
+  const array = [];
+  // var와 let의 차이점
+  for (let i = 0; i < 5; i++) {
+    array.push(() => {
+      console.log(i);
+    });
+  }
+
+  return array;
+}
+
+const array = loop();
+array.forEach((func) => func());
